2feecbe7755ed4e907fc9158a5ad759657ca8a45
Maximiliano Schvindt <schvindt@gmail.com>
2020-08-25 08:54:09 -0300

    Added datepicker

--- README.md
+++ README.md
@@ -2,4 +2,10 @@ Web Scraper para Supermercado COTO
 
 Links con alguna ayuda:
 
-    https://hasura.io/docs/1.0/graphql/manual/auth/authentication/unauthenticated-access.html 
+    https://hasura.io/docs/1.0/graphql/manual/auth/authentication/unauthenticated-access.html
+    docker exec -it scraper_client_1 bash
+    https://www.docker.com/blog/how-to-use-the-official-nginx-docker-image/
+    https://dev.to/aminnairi/quick-web-server-with-nginx-on-docker-compose-43ol
+    https://www.npmjs.com/package/vuejs-datepicker
+    https://developer.mozilla.org/en-US/docs/Mozilla/Performance/Scroll-linked_effects
+ 
--- ui/index.js
+++ ui/index.js
@@ -2,14 +2,17 @@ Vue.use(vuescroll);
 
 var cotoprices = new Vue({
     el: '#cotoprices',
+    components: {
+        vuejsDatepicker
+    },
     data: {
         prices: [],
         filterDate: dayjs().format("YYYY-MM-DD"),
         ops: {
             rail: {
-                opacity: '0.1',
-                background: undefined,
-                border: '0px solid #cecece',
+                opacity: '0.2',
+                background: '#F92672',
+                border: '0px',
                 size: '6px'
             },
             bar: {
@@ -30,29 +33,53 @@ var cotoprices = new Vue({
                 wheelScrollDuration: 0,
                 wheelDirectionReverse: false
             }
+        },
+        datepicker: {
+            date: dayjs().toDate(),
+            language: {
+                es: vdp_translation_es.js,
+            },
+            disabledDates: {
+                to: dayjs('2020-05-20').toDate(),
+                from: dayjs().toDate()
+            }
         }
     },
     mounted () {
-        let query = 'query MyQuery ($date:date){' +
+        this.getVariations();
+    },
+    methods: {
+        getVariations: function(){
+            let query = 'query MyQuery ($date:date){' +
                 '   variation(order_by: {diff: desc}, where: {date: {_eq: $date}}) {' +
                 '       date,productshopid,price,diff,product {name,url}' +
                 '   }' +
                 '}';
 
-        const url = "http://ec2-3-17-64-92.us-east-2.compute.amazonaws.com:8085/v1/graphql";
+            const url = "http://ec2-3-17-64-92.us-east-2.compute.amazonaws.com:8085/v1/graphql";
 
-        const opts = {
-            method: "POST",
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify({ query: query, variables: {"date":this.filterDate} })
-        };
+            const opts = {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({ query: query, variables: {"date":this.filterDate} })
+            };
 
-        fetch(url, opts)
-            .then(res => res.json())
-            .then(res => {this.prices = res.data.variation;})
-            .catch(console.error);
-    },
-    methods: {
+            fetch(url, opts)
+                .then(res => res.json())
+                .then(res => {this.prices = res.data.variation;})
+                .catch(console.error);
+        },
+        formatPrice: function(number) {
+            let val = (number/1).toFixed(2);
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        },
+        getPercent: function(price, diff) {
+            let previous = price - diff;
+
+            if(previous == 0) return 0.00;
+
+            return (diff / previous * 100).toFixed(2);
+        },
         twoDecimals: function(number) {
             return number.toFixed(2);
         },
@@ -60,6 +87,15 @@ var cotoprices = new Vue({
             if(number > 0) return 'diffPositive'
 
             return 'diffNegative'
+        },
+        openPicker: function() {
+            this.$refs.programaticOpen.showCalendar();
+            this.$refs.programaticOpen.$el.querySelector('input').focus();
+        },
+        dateSelected: function(date) {
+            this.prices = [];
+            this.filterDate = dayjs(date).format("YYYY-MM-DD");
+            this.getVariations();
         }
     }
 });
